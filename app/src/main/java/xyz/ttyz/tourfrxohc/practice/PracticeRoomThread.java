ed79a6b59f7c90dc3e1e2e1ca46f2e70ca8d7a6c
方志远 <343315792@qq.com>
2021-07-11 12:42:45 +0800

    ..

--- app/src/main/java/xyz/ttyz/tourfrxohc/practice/PracticeRoomThread.java
+++ app/src/main/java/xyz/ttyz/tourfrxohc/practice/PracticeRoomThread.java
@@ -1,13 +1,51 @@
 package xyz.ttyz.tourfrxohc.practice;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class PracticeRoomThread {
+import xyz.ttyz.tourfrxohc.models.UserModel;
+import xyz.ttyz.tourfrxohc.models.game.HomeModel;
+import xyz.ttyz.tourfrxohc.utils.DefaultUtils;
+import xyz.ttyz.tourfrxohc.utils.UserUtils;
 
+public class PracticeRoomThread {
+    HomeModel homeModel;
     Timer timer;
     public PracticeRoomThread() {
+        homeModel = new HomeModel();
+        //人物分配完成
+        initPerson();
+        //角色分配完成后，开启线程
 
+    }
+    /**
+     * 成员初始
+     * */
+    private void initPerson(){
+        List<UserModel> personList = new ArrayList<UserModel>();
+        UserModel localUser = UserUtils.getCurUserModel();
+        personList.add(localUser);
+        for(int i = 1; i < DefaultUtils.roomLimitMaxNumber; i ++){
+            UserModel machineUser = new UserModel();
+            machineUser.setId(i * 999);
+            machineUser.setNickname("mac_" + i);
+            personList.add(machineUser);
+        }
+        homeModel.setRoomUserList(personList);
+    }
+
+    /**
+     * 角色初始
+     * 1/3 为保底角色数量
+     * */
+    private void initRoleType(){
+        int limitNumber =  (int)(homeModel.getLimitNumber()/3);
+        //随机选择limitNumber作为保底A角色
+        // TODO: 2021/6/18  
+        //随机选择保底B角色
+        //其余人员随机分配角色
     }
 
     /*
--- app/src/main/java/xyz/ttyz/tourfrxohc/utils/DefaultUtils.java
+++ app/src/main/java/xyz/ttyz/tourfrxohc/utils/DefaultUtils.java
@@ -3,6 +3,6 @@ package xyz.ttyz.tourfrxohc.utils;
 public class DefaultUtils {
     public static long roomId;
     public static final int roomLimitMinNumber = 4;
-    public static final int roomLimitMaxNumber = 9;
+    public static final int roomLimitMaxNumber = 9;//初版默认人数
     public static final int backPutTime = 10;//钥匙放置时间
 }
